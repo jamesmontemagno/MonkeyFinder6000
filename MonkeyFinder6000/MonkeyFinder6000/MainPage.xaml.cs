ce7808534483764524ddbc618a8387925fb49ae3
adham ali <adhamali_4500@outlook.com>
2018-08-15 21:27:05 +0200

    Changes: -Adding a refresh command for uwp since not all Windows10 devices support sliding down to refresh
    -Updating the nugets.

--- MonkeyFinder6000/MonkeyFinder6000/MainPage.xaml.cs
+++ MonkeyFinder6000/MonkeyFinder6000/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonkeyFinder6000.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,24 @@ namespace MonkeyFinder6000
 {
     public partial class MainPage : ContentPage
     {
+        MonkeysViewModel viewModel;
         public MainPage()
         {
             InitializeComponent();
+
+            viewModel = new MonkeysViewModel();
+            BindingContext = viewModel;
+
+            //some Win10 devices still doesn't support sliding to refreshing
+            if(Device.RuntimePlatform == Device.UWP)
+            {
+                ToolbarItems.Add(new ToolbarItem("Refresh", "Assets/refresh.png", () =>
+                {
+                    if (viewModel.GetMonkeysCommand.CanExecute(null))
+                        viewModel.GetMonkeysCommand.Execute(null);
+
+                }));
+            }
         }
     }
 }
