6a2839c6c6bc9d618aa22cbe5d64cf8081a90b43
James Montemagno <james.montemagno@gmail.com>
2018-09-19 15:28:04 -0700

    Cleanup app and update nugets

--- MonkeyFinder6000/MonkeyFinder6000/MainPage.xaml.cs
+++ MonkeyFinder6000/MonkeyFinder6000/MainPage.xaml.cs
@@ -1,32 +1,22 @@
 ﻿using MonkeyFinder6000.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MonkeyFinder6000
 {
     public partial class MainPage : ContentPage
     {
-        MonkeysViewModel viewModel;
         public MainPage()
         {
             InitializeComponent();
 
-            viewModel = new MonkeysViewModel();
-            BindingContext = viewModel;
-
-            //some Win10 devices still doesn't support sliding to refreshing
             if(Device.RuntimePlatform == Device.UWP)
             {
-                ToolbarItems.Add(new ToolbarItem("Refresh", "Assets/refresh.png", () =>
+                ToolbarItems.Add(new ToolbarItem()
                 {
-                    if (viewModel.GetMonkeysCommand.CanExecute(null))
-                        viewModel.GetMonkeysCommand.Execute(null);
-
-                }));
+                    Text = "Refresh",
+                    Icon = "Assets/refresh.png",
+                    Command = ((MonkeysViewModel)BindingContext).GetMonkeysCommand
+                });
             }
         }
     }
